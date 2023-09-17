67808f244ba22884780468ef0d2b8b3a0af0e3d8
Guilherme <guilherme@cargon.com.br>
2023-09-17 16:16:23 -0300

    feat: ensure LogControllerDecorator returns the same value of the controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -11,7 +11,7 @@ describe('LogControllerDecorator', () => {
       return { body: '', statusCode: 500 }
     }
   }
-  test('', async () => {
+  test('Should call Controller with correct request values', async () => {
     const controllerTest = new ControllerTest()
     const logController = new LogControllerDecorator(controllerTest)
 
@@ -29,5 +29,23 @@ describe('LogControllerDecorator', () => {
     await logController.handle(request)
 
     expect(spyController).toHaveBeenCalledWith(request)
+  })
+
+  test('Should return the same value of the controller', async () => {
+    const controllerTest = new ControllerTest()
+    const logController = new LogControllerDecorator(controllerTest)
+
+    const request = {
+      body: {
+        email: 'guiflr@gmail.com',
+        name: 'gui',
+        password: '123',
+        passwordConfirmation: '123'
+      }
+    }
+
+    const logResponse = await logController.handle(request)
+
+    expect(logResponse).toEqual({ body: '', statusCode: 500 })
   })
 })
